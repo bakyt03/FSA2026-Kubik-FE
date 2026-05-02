14cecb0d6e8c2c83558110eca69fb05087438de1
bakyt03 <kubiklukas007@gmail.com>
2026-05-02 15:40:33 +0200

    fix keycloak /auth prefix in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keyCloakUrl: 'https://keycloak.20.234.120.137.nip.io',
+  keyCloakUrl: 'https://keycloak.20.234.120.137.nip.io/auth',
   beUrl: 'https://app.20.234.120.137.nip.io',
   appUrl: 'https://app.20.234.120.137.nip.io',
 };
