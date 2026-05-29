6d6362b28a5415ec68ec6361fd8babbd4bb4983a
bakyt03 <kubiklukas007@gmail.com>
2026-05-29 12:37:47 +0200

    added silent refresh to fix PROD issue

--- src/app/core/auth-code-flow.config.ts
+++ src/app/core/auth-code-flow.config.ts
@@ -4,9 +4,14 @@ import { environment } from '../../environments/environment';
 export const authCodeFlowConfig: AuthConfig = {
   issuer: environment.keyCloakUrl + '/realms/FSA',
   redirectUri: environment.appUrl + '/home',
+  silentRefreshRedirectUri: environment.appUrl + '/home',
   clientId: 'fsa-client',
   responseType: 'code',
-  scope: 'openid profile email roles',
+  scope: 'openid profile email roles offline_access',
+  useSilentRefresh: true,
+  timeoutFactor: 0.75,
+  silentRefreshTimeout: 5000,
+  sessionChecksEnabled: true,
   showDebugInformation: true,
   requireHttps: false,
 };
